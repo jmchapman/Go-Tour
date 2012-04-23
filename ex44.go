027d1c70d5a215bbb53b77b08f64e626ce86bd4a
James Chapman <jmchapman@gmail.com>
2012-04-23 14:36:38 +0300

    added ex44, same problem about the import I guess

---
+++ ex44.go
@@ -0,0 +1,18 @@
+package main
+
+import "go-tour.googlecode.com/hg/pic"
+
+func Pic(dx, dy int) (p [][]uint8) {
+     p = make([][]uint8,dy)
+     for y := 0; y < dy; y++ {
+     	 p[y] = make([]uint8,dx)
+	      for x := 0; x < dx; x++ {
+	      	    p[y][x] = uint8(x ^ y)
+		    	    }
+			    }
+			    return
+}
+
+func main() {
+     pic.Show(Pic)
+}
